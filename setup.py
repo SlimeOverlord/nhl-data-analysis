c3e76e978d0e4aac249ee5fbc4a7e4faaa400b9c
SlimeOverlord <nikkollerv@gmail.com>
2025-09-21 21:53:44 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='ift6758',
+    name='NHL Data Analysis',
     packages=find_packages(),
     # package_dir={'': 'src'},
     version='0.1.0',
-    description='Sample project repo for IFT6758-2021',
-    author='[FILL IN TEAM MEMBERS]',
+    description='An analysis on real-life NHL data',
+    author='Alexandre Dewilde, Ayman Khechman, Natali Mercado, Nicolas Ollervides',
     license='',
 )
